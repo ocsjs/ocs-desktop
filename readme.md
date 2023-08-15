6d29148c6787f41e194c1df5a728ad9d6f56f1b7
enncy <877526278@qq.com>
2023-08-15 21:50:11 +0800

    build: 优化项目架构文件

--- readme.md
+++ readme.md
@@ -1,3 +1,25 @@
 # ocs-desktop
 
 ocs 软件
+
+## 运行
+
+```bash
+# 安装依赖
+npm i pnpm -g
+pnpm install
+
+# 打开两个终端
+# 终端1
+cd packages/web
+npm run dev
+# 终端2
+cd packages/app
+npm run dev
+```
+
+## 打包
+
+```bash
+npm run build
+```
--- scripts/tsc.js
+++ scripts/tsc.js
@@ -6,7 +6,5 @@ exports.default = series(
 		() => execOut('tsc', { cwd: '../packages/common' }),
 		() => execOut('tsc', { cwd: '../packages/app' }),
 		() => execOut('tsc', { cwd: '../packages/web' }),
-		() => execOut('tsc', { cwd: '../packages/utils' }),
-		() => execOut('tsc', { cwd: '../packages/core' })
 	)
 );
