665826ad84a02876bbb2847b4ea110ffeb8a7b33
enncy <877526278@qq.com>
2025-05-13 01:53:26 +0800

    perf: 在软件设置浏览器路径中添加自动浏览器路径填写提示菜单

--- packages/web/src/components/setting/BrowserPath.vue
+++ packages/web/src/components/setting/BrowserPath.vue
@@ -1,37 +1,48 @@
 <template>
 	<Description label="浏览器路径">
-		<a-input
-			v-model="launchOptions.executablePath"
-			size="small"
-			placeholder="输入您的浏览器路径，否则无法正常启动，路径获取教程点击右侧问号查看。"
-			class="w-100"
-			@change="onDiy"
-		>
-			<template #suffix>
-				<a-popover>
-					<template #title>
-						<b>浏览器路径获取方式</b>
-					</template>
-					<template #content>
-						<div>
-							<b>- 谷歌浏览器</b> : 打开谷歌浏览器 <br />
-							在地址栏输入
-							<b>chrome://version</b> 并回车， 找到 <b>可执行文件路径</b> 复制粘贴即可
-						</div>
-						<div>
-							<b>- Edge浏览器</b> : 打开Edge浏览器<br />
-							在地址栏输入
-							<b>edge://version</b> 并回车， 找到 <b>可执行文件路径</b> 复制粘贴即可
-						</div>
-					</template>
+		<a-dropdown trigger="hover">
+			<a-input
+				v-model="launchOptions.executablePath"
+				size="small"
+				placeholder="输入您的浏览器路径，否则无法正常启动，路径获取教程点击右侧问号查看。"
+				class="w-100"
+				@change="onDiy"
+			>
+				<template #suffix>
+					<a-popover>
+						<template #title>
+							<b>浏览器路径获取方式</b>
+						</template>
+						<template #content>
+							<div>
+								<b>- 谷歌浏览器</b> : 打开谷歌浏览器 <br />
+								在地址栏输入
+								<b>chrome://version</b> 并回车， 找到 <b>可执行文件路径</b> 复制粘贴即可
+							</div>
+							<div>
+								<b>- Edge浏览器</b> : 打开Edge浏览器<br />
+								在地址栏输入
+								<b>edge://version</b> 并回车， 找到 <b>可执行文件路径</b> 复制粘贴即可
+							</div>
+						</template>
 
-					<Icon
-						type="help_outline"
-						style="cursor: pointer"
-					/>
-				</a-popover>
+						<Icon
+							type="help_outline"
+							style="cursor: pointer"
+						/>
+					</a-popover>
+				</template>
+			</a-input>
+			<template #content>
+				<a-doption
+					v-for="item of state.validBrowsers"
+					:key="item.path"
+					@click="launchOptions.executablePath = item.path"
+				>
+					{{ item.name }}
+				</a-doption>
 			</template>
-		</a-input>
+		</a-dropdown>
 	</Description>
 </template>
 
@@ -42,8 +53,19 @@ import { remote } from '../../utils/remote';
 import Description from '../Description.vue';
 import Icon from '../Icon.vue';
 import { forceClearBrowserCache } from '../../utils/browser';
+import { onMounted, reactive } from 'vue';
+import { ValidBrowser } from '@ocs-desktop/common/lib/src/interface';
 
 const launchOptions = store.render.setting.launchOptions;
+
+const state = reactive({
+	validBrowsers: [] as ValidBrowser[]
+});
+
+onMounted(async () => {
+	state.validBrowsers = await remote.methods.call('getValidBrowsers');
+	console.log(state.validBrowsers);
+});
 
 /**
  * 监听自定义浏览器编辑
