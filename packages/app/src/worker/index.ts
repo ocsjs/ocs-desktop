2abcf6a6599ca53eb70e6318bfbaabf1b7775354
enncy <877526278@qq.com>
2025-10-02 00:36:18 +0800

    fix: 修复油猴拓展无法降级的BUG

--- packages/app/src/worker/index.ts
+++ packages/app/src/worker/index.ts
@@ -485,7 +485,7 @@ async function initScripts(urls: string[], browser: BrowserContext, config?: Bro
 						btn?.click();
 					} else {
 						if (
-							['更新', '安装', '添加', 'install', 'update', 'add'].some(
+							['更新', '安装', '添加', '降级', 'install', 'update', 'add'].some(
 								(text) =>
 									(btn?.textContent || btn?.getAttribute('value') || '').trim().toLocaleLowerCase() ===
 									text.trim().toLocaleLowerCase()
