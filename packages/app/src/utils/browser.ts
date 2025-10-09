27d7f208d14feff9e0786166a30f1262903f1a42
enncy <877526278@qq.com>
2025-10-09 16:40:21 +0800

    fix: 修复环境监测时读取本地路径为空报错的BUG

--- packages/app/src/utils/browser.ts
+++ packages/app/src/utils/browser.ts
@@ -6,6 +6,8 @@ import path from 'path';
  * @param executablePath 浏览器可执行文件路径
  */
 export function getBrowserMajorVersion(executablePath: string) {
+	if (fs.existsSync(executablePath) === false) return;
+
 	let manifest = fs.readdirSync(path.dirname(executablePath)).find((f) => {
 		const file = path.join(path.dirname(executablePath), f);
 		return fs.statSync(file).isDirectory() && fs.readdirSync(file).some((f) => f.endsWith('.manifest'));
